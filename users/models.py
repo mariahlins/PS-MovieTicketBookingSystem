5546cdb4df2392e9eebef99eff35714a64140ae4
mariahlins <mariahlinssena4@gmail.com>
2024-07-29 13:51:40 -0300

    novos campos de registro

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
+# models.py
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    birth_date = models.DateField(null=True, blank=True)
+    
+    def __str__(self):
+        return f"{self.user.username}: {self.user.email}"
